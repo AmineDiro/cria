e98ec6e3f907c84ea64ca79f2778b5f36ccb1da6
amine dirhoussi <aminedirhoussi@amines-MacBook-Air.local>
2023-08-08 16:53:43 +0200

    small fix langchain

--- python/langchain_openai.py
+++ python/langchain_openai.py
@@ -1,14 +1,9 @@
 import os
 from langchain.llms import OpenAI
 from langchain import PromptTemplate, LLMChain
+
 os.environ["OPENAI_API_KEY"] = "api_key"
 
-
-template = """
-USER: You are a helpful, medical specialist. Always answer as helpfully as possible.  If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information. You always answer medical questions based on facts.
-ASSISTANT: Ok great ! I am a medical expert!
-USER: {question} [/INST]
-"""
 
 template = """
 USER: You are a helpful, medical specialist. Always answer as helpfully as possible.  If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information. You always answer medical questions based on facts.
