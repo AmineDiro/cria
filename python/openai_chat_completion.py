a589c8e7bfd8450d5e0d0cc5f9d8172df3154601
amine dirhoussi <aminedirhoussi@amines-MacBook-Air.local>
2023-08-08 16:52:59 +0200

    added openai chat example

---
+++ python/openai_chat_completion.py
@@ -0,0 +1,16 @@
+import openai
+
+openai.organization = "YOUR_ORG_ID"
+openai.api_key = "test"
+openai.api_base = "http://localhost:3000/v1"
+
+
+completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Hello!"},
+    ],
+)
+
+print(completion.choices[0].message)
